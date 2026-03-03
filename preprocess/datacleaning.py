7734864c03a3383377b2865db17f13c286cf3bc2
Harry <hdreynolds99@gmail.com>
2026-03-03 17:33:27 +0000

    unnamed column handling wop

--- preprocess/datacleaning.py
+++ preprocess/datacleaning.py
@@ -1,6 +1,33 @@
-def rename_headers():
-    # Rename df headers
-    pass
+def rename_headers(df):
+
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace(' ', '_')
+    df.columns = df.columns.str.strip()
+
+    return df
+
+def unnamed_col_handling(df):
+
+
+
+    cols = df.columns
+    unnamed_cols = []
+    new_list = []
+
+    for col in cols:
+        uniques = df[col].unique()
+        if col.startswith('unnamed'):
+            unnamed_cols.append(col)
+
+    unnamed_df = df[unnamed_cols]
+
+    for col in unnamed_df.columns:
+        unnamed_df_unique = unnamed_df[col].unique()
+        if unnamed_df_unique.i:
+            new_list.append(col)
+
+
+    return new_list
 
 def merge_dfs(df1, df2):
     # Merge datasets
