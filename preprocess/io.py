3bed54d59e2d9287e7db396fb301e74db1f49b1a
shane <shanebrodigan1@gmail.com>
2026-03-03 16:28:40 +0000

    notebook integration problem

--- preprocess/io.py
+++ preprocess/io.py
@@ -1,9 +1,15 @@
-class Reader:
-    def read_in(self, path):
-        # read in csv
-        pass
+import pandas as pd
 
+class Reader:
+
+    def read_in(self, file_path):
+        df = pd.read_csv(file_path)
+        return df
+
+
+"""
 class Writer:
     def write_out(self, DataFrame):
         # write out dataframe
-        pass
+        d = 2
+"""
