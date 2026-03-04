00e2da39770dd831999b360848f777801d3c6c93
Harry <hdreynolds99@gmail.com>
2026-03-04 13:38:16 +0000

    drop_unnamed_cols() function implemented

--- preprocess/datacleaning.py
+++ preprocess/datacleaning.py
@@ -1,37 +1,22 @@
 def rename_headers(df):
-
     df.columns = df.columns.str.lower()
     df.columns = df.columns.str.replace(' ', '_')
     df.columns = df.columns.str.strip()
 
     return df
 
-def unnamed_col_handling(df):
 
+def drop_unnamed_cols(df):
+    unnamed_cols = df.columns[df.columns.str.startswith('unnamed')]
+    no_unnamed_cols_df = df.drop(unnamed_cols, axis=1)
 
+    return no_unnamed_cols_df
 
-    cols = df.columns
-    unnamed_cols = []
-    new_list = []
-
-    for col in cols:
-        uniques = df[col].unique()
-        if col.startswith('unnamed'):
-            unnamed_cols.append(col)
-
-    unnamed_df = df[unnamed_cols]
-
-    for col in unnamed_df.columns:
-        unnamed_df_unique = unnamed_df[col].unique()
-        if unnamed_df_unique.i:
-            new_list.append(col)
-
-
-    return new_list
 
 def merge_dfs(df1, df2):
     # Merge datasets
     pass
+
 
 class NaHandler():
     def dropna(self):
