d18038f43733afda7ee17fa92dd2439fd6ee9926
Harry <hdreynolds99@gmail.com>
2026-03-10 13:40:35 +0000

    Added comments to __main__.py

--- __main__.py
+++ __main__.py
@@ -9,31 +9,41 @@ from preprocess import feature_engineering
 
 def main():
     config = Config()
-
     reader = Reader()
+
+    # Read in Csv files.
     df_1 = reader.read_in('./datasets/AppGallery.csv')
     df_2 = reader.read_in('./datasets/Purchasing.csv')
 
+    # Renamed headers and dropped unnamed cols.
     df_1 = dc.rename_headers(df_1)
     df_1 = dc.drop_unnamed_cols(df_1)
 
     df_2 = dc.rename_headers(df_2)
     df_2 = dc.drop_unnamed_cols(df_2)
 
+    # Merged dataframes.
     merged = dc.merge_dfs([df_1, df_2])
 
+    # Converted all text in dataframe to lowercase.
     df = dc.col_rows_to_lowercase(merged)
 
+    # Handled null values.
     na_handler = dc.NaHandler(essential_col_names=config.ESSENTIAL_COLS)
     df = na_handler.drop_na_rows(df)
 
+    # Dropped useless columns.
     df = dc.drop_useless_cols(df)
+
+    # Translated dataframe to english.
+    # Also, cleaned dataframe to get rid of any noise prior creating word embeddings.
 
     # df = dc.translate_to_en(df)
     # writer = Writer()
     # writer.write_out(df, './translated_df.csv')
     df = reader.read_in('./translated_df.csv')
 
+    # Feature Engineered Dataset.
     fe = feature_engineering.FeatureEngineering(df)
     df = fe.process_data()
 
